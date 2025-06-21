6fec0066f4938784e90227d736273726d10c4270
herman-xphp <hermanxphp@gmail.com>
2025-06-21 10:41:12 +0800

    dashboard page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./components/Layout";
 import UserRegister from "./components/Users/UserRegister";
 import UserLogin from "./components/Users/UserLogin";
+import DashboardLayout from "./components/DashboardLayout";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,7 +14,9 @@ createRoot(document.getElementById("root")).render(
           <Route path="/register" element={<UserRegister />} />
           <Route path="/login" element={<UserLogin />} />
         </Route>
-        <Route path="/dashboard"></Route>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="contacts" element={<div>Contact</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
