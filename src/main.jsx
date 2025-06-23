e59c9f93b46781f3afe9efcf444c741b8ebb616e
herman-xphp <hermanxphp@gmail.com>
2025-06-23 18:33:43 +0800

    create contact page

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import UserLogin from "./components/Users/UserLogin";
 import DashboardLayout from "./components/DashboardLayout";
 import UserProfile from "./components/Users/UserProfile";
 import UserLogout from "./components/Users/UserLogout";
+import ContactCreate from "./components/Contact/ContactCreate";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -17,9 +18,15 @@ createRoot(document.getElementById("root")).render(
           <Route path="/login" element={<UserLogin />} />
         </Route>
         <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route path="contacts" element={<div>Contact</div>} />
-          <Route path="users/profile" element={<UserProfile />} />
-          <Route path="users/logout" element={<UserLogout />} />
+          <Route path="users">
+            <Route path="profile" element={<UserProfile />} />
+            <Route path="logout" element={<UserLogout />} />
+          </Route>
+
+          <Route path="contacts">
+            <Route index element={<div>Contact</div>} />
+            <Route path="create" element={<ContactCreate />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
